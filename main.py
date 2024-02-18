6cc4b621fa8dce817f9ae45f59c93bcc0ebc5040
Artemy Ivanov <ghostsmans@gmail.com>
2024-02-18 13:54:54 +0300

    Optimized search for max element and added mypy to requirements.txt

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ class Jacobi:
         self.iterations = 0
         self.a = []
         self.height = len(a)
-        self.s = 0
-        self.v = [[1 for _ in range(self.height)].copy() for _ in range(self.height)]
+        self.s = 0.
+        self.v = [[1. for _ in range(self.height)] for _ in range(self.height)]
 
         flag = True
         row0len = len(a)
@@ -27,12 +27,12 @@ class Jacobi:
         assert flag, "Matrix does not have consistent dimensions"
 
         for row in a:
-            self.s += sum([i**2 for i in row])
+            self.s += sum([i ** 2 for i in row])
             self.a.append(row.copy())
 
     def gen_p(self, phi: Phi, p: int, q: int):
         assert p != q, "p equals q"
-        temp_p = [[0 for _ in range(self.height)].copy() for _ in range(self.height)]
+        temp_p = [[0. for _ in range(self.height)] for _ in range(self.height)]
         for i in range(self.height):
             temp_p[i][i] = 1
         temp_p[p][p] = temp_p[q][q] = phi.cos
@@ -49,20 +49,15 @@ class Jacobi:
             pass
         c = top / bot
         if c >= 0:
-            tgphi = 1 / (c + sqrt(c**2 + 1))
+            tgphi = 1 / (c + sqrt(c ** 2 + 1))
         else:
-            tgphi = 1 / (c - sqrt(c**2 + 1))
-        cosphi = 1 / (sqrt(1 + tgphi**2))
+            tgphi = 1 / (c - sqrt(c ** 2 + 1))
+        cosphi = 1 / (sqrt(1 + tgphi ** 2))
         sinphi = tgphi * cosphi
         return Phi(tgphi, cosphi, sinphi)
 
     def zero_element(self, p: int, q: int):
-        new_field = []
-        for i in range(self.height):
-            new_field.append([])
-            for j in range(self.height):
-                new_field[i].append(self.a[i][j])
-
+        new_field = deepcopy(self.a)
         phi = self.calc_phi(p, q)
         new_field[p][q] = new_field[q][p] = 0
         new_field[p][p] = self.a[p][p] - self.a[p][q] * phi.tgn
@@ -72,35 +67,35 @@ class Jacobi:
                 continue
             if i != p:
                 new_field[i][p] = new_field[p][i] = self.a[i][p] - phi.sin * (
-                    self.a[i][q] + phi.sin / (1 + phi.cos) * self.a[i][p]
+                        self.a[i][q] + phi.sin / (1 + phi.cos) * self.a[i][p]
                 )
                 self.v[i][p] = self.v[i][p] * phi.cos - self.v[i][q] * phi.sin
             if i != q:
                 new_field[i][q] = new_field[q][i] = self.a[i][q] + phi.sin * (
-                    self.a[i][p] - phi.sin / (1 + phi.cos) * self.a[i][q]
+                        self.a[i][p] - phi.sin / (1 + phi.cos) * self.a[i][q]
                 )
                 self.v[i][q] = self.v[i][p] * phi.sin + self.v[i][q] * phi.cos
                 pass
-        self.s -= 2*self.a[p][q] ** 2
+        self.s -= 2 * self.a[p][q] ** 2
         self.a = deepcopy(new_field)
 
     def iterate(self):
         p, q = 0, 1
         for i in range(self.height):
-            for j in range(self.height):
-                if abs(self.a[i][j]) > abs(self.a[p][q]) and i != j:
+            for j in range(i + 1, self.height):
+                if abs(self.a[i][j]) > abs(self.a[p][q]):
                     p, q = i, j
         self.zero_element(p, q)
 
 
 if __name__ == "__main__":
-    a = [[i for i in range(j, j + 3 * 5, 3)] for j in range(1, 14, 3)]
+    a = [[float(i) for i in range(j, j + 3 * 5, 3)] for j in range(1, 14, 3)]
     test = Jacobi(a)
     phi = test.calc_phi(1, 3)
     p_cur = test.gen_p(phi, 1, 3)
     for i in range(20):
         test.iterate()
-        print(f"\t\tIteration №{i+1}\n\t\tS = {test.s}")
+        print(f"\t\tIteration №{i + 1}\n\t\tS = {test.s}")
         for row in test.a:
             print(*[f"{i:.2f}" for i in row], sep="\t")
     pass
